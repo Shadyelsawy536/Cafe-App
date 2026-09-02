33a6600715fa091b9975a84f93656bfc1fc20890
Shady Elsawy <106924801+Shadyelsawy536@users.noreply.github.com>
2026-09-02 23:28:10 +0300

    feat: connect Flutter to live dashboard configuration

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/config/supabase_config.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/presentation/controllers/auth_controller.dart';
+import 'features/ordering/data/dashboard_config_realtime.dart';
 import 'features/ordering/data/product_repository.dart';
 import 'features/ordering/data/supabase_product_repository.dart';
 import 'features/ordering/presentation/controllers/ordering_controller.dart';
@@ -19,18 +20,59 @@ Future<void> main() async {
   runApp(const CafeApp());
 }
 
-class CafeApp extends StatelessWidget {
+class CafeApp extends StatefulWidget {
   const CafeApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final ProductRepository repository = SupabaseProductRepository();
+  State<CafeApp> createState() => _CafeAppState();
+}
 
+class _CafeAppState extends State<CafeApp> {
+  late final ProductRepository _repository;
+  late final OrderingController _controller;
+  late final DashboardConfigRealtime _configRealtime;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _repository = SupabaseProductRepository();
+    _controller = OrderingController(repository: _repository);
+
+    _configRealtime = DashboardConfigRealtime(
+      repository: _repository,
+      onSettings: (settings) {
+        if (!mounted) return;
+        _controller.settings = settings;
+        _controller.notifyListeners();
+      },
+      onBranding: (branding) {
+        if (!mounted) return;
+        _controller.branding = branding;
+        _controller.notifyListeners();
+      },
+      onLocations: (locations) {
+        if (!mounted) return;
+        _controller.locations = locations;
+        _controller.notifyListeners();
+      },
+    )..start();
+
+    _controller.loadData();
+  }
+
+  @override
+  void dispose() {
+    _configRealtime.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => OrderingController(repository: repository)..loadData(),
-        ),
+        ChangeNotifierProvider.value(value: _controller),
         ChangeNotifierProvider(create: (_) => AuthController()),
       ],
       child: Consumer<OrderingController>(
